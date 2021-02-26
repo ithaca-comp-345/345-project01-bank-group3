1c0f723853b69b284099b0d9c8741d536855e95c
kcallaghan1 <47750665+kcallaghan1@users.noreply.github.com>
2021-02-26 08:52:33 -0500

    Changed withdraw method for Savings account

--- src/main/java/edu/ithaca/dragon/bank/SavingsAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/SavingsAccount.java
@@ -19,5 +19,24 @@ public class SavingsAccount extends BankAccount{
         balance += added;
     }
 
+    @Override
+    public void withdraw (double amount) throws InsufficientFundsException, IllegalArgumentException{
+        if(isAmountValid(amount) == false){
+            throw new IllegalArgumentException("Invalid amount");
+        }
+        else if (amount <= balance && remainingWithdrawLimit - amount >= 0){
+            remainingWithdrawLimit -= amount;
+            balance -= amount;
+        }
+        else {
+            throw new InsufficientFundsException("Not enough money");
+        }
+    }
+    }
+
+    protected void resetWithdrawLimit(){
+        remainingWithdrawLimit = withdrawLimit;
+    }
+
     
 }
