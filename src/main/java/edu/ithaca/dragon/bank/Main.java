b58181276e2e9ad289adee00802bce46b4e1ed4b
Pax <pmeny@ithaca.edu>
2021-02-28 14:33:33 -0500

    Main fixed main method syntax

--- src/main/java/edu/ithaca/dragon/bank/Main.java
+++ src/main/java/edu/ithaca/dragon/bank/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main(){
 
-    public static void main(string args[]){
+    public static void main(String[] args){
         UI ui = new UI();
         ui.logIn();
         remove(ui);
